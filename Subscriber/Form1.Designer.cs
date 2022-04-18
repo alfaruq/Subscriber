c759d7d98a1b0ff0d74d95920ca4082944b5e524
alfaruq <alfaruq@DESKTOP-6UTB36S>
2022-04-19 03:32:21 +0800

    Add project files.

---
+++ Subscriber/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Subscriber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTriggerTime = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMaxMessages = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtQueueURL = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnPull = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnPull);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtQueueURL);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtMaxMessages);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtTriggerTime);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 188);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Configuration";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Trigger Time";
+            // 
+            // txtTriggerTime
+            // 
+            this.txtTriggerTime.Location = new System.Drawing.Point(111, 28);
+            this.txtTriggerTime.Name = "txtTriggerTime";
+            this.txtTriggerTime.Size = new System.Drawing.Size(142, 20);
+            this.txtTriggerTime.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Max Messages";
+            // 
+            // txtMaxMessages
+            // 
+            this.txtMaxMessages.Location = new System.Drawing.Point(111, 61);
+            this.txtMaxMessages.Name = "txtMaxMessages";
+            this.txtMaxMessages.Size = new System.Drawing.Size(142, 20);
+            this.txtMaxMessages.TabIndex = 3;
+            this.txtMaxMessages.Text = "10";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "URI";
+            // 
+            // txtQueueURL
+            // 
+            this.txtQueueURL.Location = new System.Drawing.Point(111, 94);
+            this.txtQueueURL.Name = "txtQueueURL";
+            this.txtQueueURL.Size = new System.Drawing.Size(590, 20);
+            this.txtQueueURL.TabIndex = 5;
+            this.txtQueueURL.Text = "https://sqs.ap-southeast-1.amazonaws.com/551639573710/demo-std-queue";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Subscriber Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(111, 127);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(142, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "FooBar1";
+            // 
+            // btnPull
+            // 
+            this.btnPull.Location = new System.Drawing.Point(695, 159);
+            this.btnPull.Name = "btnPull";
+            this.btnPull.Size = new System.Drawing.Size(75, 23);
+            this.btnPull.TabIndex = 8;
+            this.btnPull.Text = "Pull";
+            this.btnPull.UseVisualStyleBackColor = true;
+            this.btnPull.Click += new System.EventHandler(this.btnPull_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(12, 206);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResult.Size = new System.Drawing.Size(776, 307);
+            this.txtResult.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 525);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Subscriber Demo (Queue)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtTriggerTime;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtQueueURL;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMaxMessages;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnPull;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ Subscriber/Form1.cs
@@ -0,0 +1,95 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Subscriber
+{
+    public partial class Form1 : Form
+    {
+        private AmazonSQSClient _objClient;
+        private DateTime _timeTrigger;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _objClient = new AmazonSQSClient();
+            timer1.Tick += Timer1_Tick;
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (DateTime.Now >= _timeTrigger)
+            {
+                PullMessages();
+
+                btnPull.Enabled = true;
+                timer1.Stop();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            #region Set Timer
+
+            var temp = DateTime.Now.AddSeconds(30);
+            txtTriggerTime.Text = temp.ToString("HH:mm:ss");
+
+            #endregion
+        }
+
+        private void btnPull_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                btnPull.Enabled = false;
+
+                var dailyTime = txtTriggerTime.Text.Trim();
+                var timeParts = dailyTime.Split(new char[] { ':' });
+                var dateNow = DateTime.Now;
+                _timeTrigger = new DateTime(dateNow.Year, dateNow.Month, dateNow.Day,
+                    int.Parse(timeParts[0]), int.Parse(timeParts[1]), int.Parse(timeParts[2]));
+
+                timer1.Start();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void PullMessages()
+        {
+            try
+            {
+                var URL = txtQueueURL.Text.Trim();
+                ReceiveMessageResponse response = new ReceiveMessageResponse();
+
+                response = _objClient.ReceiveMessage(new ReceiveMessageRequest()
+                {
+                    QueueUrl = URL,
+                    MaxNumberOfMessages = int.Parse(txtMaxMessages.Text.Trim())
+                });
+
+                foreach(var message in response.Messages)
+                {
+                    txtResult.Text += String.Format("{0}" + System.Environment.NewLine, message.Body);
+                    _objClient.DeleteMessage(URL, message.ReceiptHandle);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
